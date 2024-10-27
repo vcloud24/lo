bbd41f024891a3cea0f32ddf626acc2a68246b4e
vcloud24 <vilakazigames@gmail.com>
2024-10-27 23:03:54 +0200

    Create app-builder.js

---
+++ app-builder.js
@@ -0,0 +1,9 @@
+function downloadApp(platform) {
+    if (platform === 'android') {
+        alert("Generating Android app...");
+        // Android app generation logic here
+    } else if (platform === 'windows') {
+        alert("Generating Windows app...");
+        // Windows app generation logic here
+    }
+}
